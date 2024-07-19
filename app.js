5c2717310fd226097041225d6bfe94526f512bca
Ignacio Guridi <nacho_g88@hotmail.com>
2024-07-19 16:59:51 -0300

    Agregado de funciones y estilos

--- app.js
+++ app.js
@@ -25,3 +25,35 @@ function scrollToHeader() {
         behavior: 'smooth'
     });
 }
+
+// Función para el desplazamiento suave con duración personalizada
+function smoothScroll(target, duration) {
+    const targetPosition = document.querySelector(target).offsetTop;
+    const startPosition = window.pageYOffset;
+    const distance = targetPosition - startPosition;
+    let startTime = null;
+
+    function animation(currentTime) {
+        if (startTime === null) startTime = currentTime;
+        const timeElapsed = currentTime - startTime;
+        const run = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+        if (timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+    function ease(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
+    }
+
+    requestAnimationFrame(animation);
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        smoothScroll(this.getAttribute('href'), 1000); // 1000 ms = 1 segundo
+    });
+});
