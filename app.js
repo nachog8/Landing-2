fa0b44bbc8c6787c54edc3bd01cd148457097da9
Alvarezsergio <alvarez.94.sergio@gmail.com>
2024-07-19 21:11:52 -0300

    update_page

--- app.js
+++ app.js
@@ -1,20 +1,52 @@
+function show_error(event, el) {
+    if (!event.target.value.trim()) {
+        el.style.display = 'block'
+    }
+    else {
+        el.style.display = 'none'
+    }
+}
+
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (() => {
     'use strict'
-  
+
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     const forms = document.querySelectorAll('.needs-validation')
-  
+    const name = document.querySelector('#validationCustomname')
+    const email = document.querySelector('#validationCustomEmail')
+    const message = document.querySelector('#validationCustomTextarea')
     // Loop over them and prevent submission
     Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-  
-        form.classList.add('was-validated')
-      }, false)
+        form.addEventListener('submit', event => {
+
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+                return
+            }
+
+            form.classList.add('was-validated')
+        }, false)
+    })
+
+    const invalid_name_container = document.getElementById('invalid_name')
+    const invalid_email_container = document.getElementById('invalid_email')
+    const invalid_message_container = document.getElementById('invalid_message')
+
+    name.addEventListener('input', event => {
+        show_error(event, invalid_name_container)
+
+    })
+
+    email.addEventListener('input', event => {
+        show_error(event, invalid_email_container)
+
+    })
+
+    message.addEventListener('input', event => {
+        show_error(event, invalid_message_container)
+
     })
 })()
 
